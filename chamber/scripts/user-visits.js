16eeae5fdaa80fdb6c49abe5a4af9508b92d3667
Rory Nye <rnye2000@hotmail.com>
2024-02-28 18:44:50 -0700

    error fixes

---
+++ chamber/scripts/user-visits.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    let message = document.getElementById('display-message');
+
+    if(checkForInitalVisit() == 0){
+        setInitialValues();
+        message.innerHTML = 'Welcome! Let us know if you have any questions.'
+    } else if (diffDates() <= 1 ){
+        message.innerHTML = 'Back so soon! Awesome!';
+    } else {
+        message.innerHTML = `You last visited ${diffDates()} days ago`
+    }
+
+});
+
+function checkForInitalVisit(){
+    return localStorage.getItem('numberOfVisits');
+}
+
+function setInitialValues(){
+    localStorage.setItem('numberOfVisits', 0);
+    localStorage.setItem('dateLastVisited', Date.now());
+}
+
+function diffDates(){
+    let lastVisit = parseInt(localStorage.getItem('dateLastVisited'));
+    let visit = Date.now();
+    return (visit - lastVisit) / (1000 * 60 * 60);
+}
